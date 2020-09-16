da6b4fcdad582a5b4d07ccfbcfd6937386f454cc
Jared Ostmeyer <jostmey@gmail.com>
2020-09-16 17:10:47 -0500

    Adding L1/2 loss options for Alignment layer

--- repertoire-classification-problem/model/Alignment.py
+++ repertoire-classification-problem/model/Alignment.py
@@ -9,24 +9,42 @@ import tensorflow.keras.backend as K
 from alignment_score import *
 
 class Alignment(Layer):
-  def __init__(self, filters, weight_steps, penalties_feature=0.0, penalties_filter=0.0, length_normalize=False, **kwargs):
+  def __init__(
+      self,
+      filters, weight_steps,
+      penalties_feature=0.0, penalties_filter=0.0, length_normalize=False,
+      kernel_initializer='uniform', bias_initializer='zeros',
+      kernel_regularizer=None, bias_regularizer=None,
+      kernel_constraint=None, bias_constraint=None,
+      **kwargs
+    ):
     self.filters = filters
     self.weight_steps = weight_steps
     self.penalties_feature = penalties_feature
     self.penalties_filter = penalties_filter
     self.length_normalize = length_normalize
+    self.kernel_initializer = kernel_initializer
+    self.bias_initializer = bias_initializer
+    self.kernel_regularizer = kernel_regularizer
+    self.bias_regularizer = bias_regularizer
+    self.kernel_constraint = kernel_constraint
+    self.bias_constraint = bias_constraint
     super(__class__, self).__init__(**kwargs)
   def build(self, input_shape):
     self.kernel = self.add_weight(
       name='kernel',
       shape=[self.weight_steps, int(input_shape[2]), self.filters],
-      initializer='uniform',
+      initializer=self.kernel_initializer,
+      regularizer=self.kernel_regularizer,
+      constraint=self.kernel_constraint,
       trainable=True
     )
     self.bias = self.add_weight(
       name='bias',
       shape=[self.filters],
-      initializer='zeros',
+      initializer=self.bias_initializer,
+      regularizer=self.bias_regularizer,
+      constraint=self.bias_constraint,
       trainable=True
     )
     super(__class__, self).build(input_shape)
